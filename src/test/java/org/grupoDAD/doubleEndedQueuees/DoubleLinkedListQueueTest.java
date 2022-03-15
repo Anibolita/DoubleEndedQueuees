ed44875e9782abca52dfc9b712491153b2d1caf3
Dariiopk <dariogarciamartin@uma.es>
2022-03-15 19:55:51 +0100

    Test case create new valid list and test peekFirst(), peekLast(), size().

--- src/test/java/org/grupoDAD/doubleEndedQueuees/DoubleLinkedListQueueTest.java
+++ src/test/java/org/grupoDAD/doubleEndedQueuees/DoubleLinkedListQueueTest.java
@@ -1,7 +1,24 @@
 package org.grupoDAD.doubleEndedQueuees;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleLinkedListQueueTest {
 
+    @Test
+    public void createValidNotNullIntegerValuesListShould(){
+        DequeNode<Integer> expectedFirstNode = new DequeNode<>(0, null, null);
+        DequeNode<Integer> expectedLastNode = new DequeNode<>(1, null, null);
+        int expectedSize = 2;
+
+        DoubleLinkedListQueue<Integer> obtainedList = new DoubleLinkedListQueue<>();
+        obtainedList.append(expectedFirstNode);
+        obtainedList.append(expectedLastNode);
+
+        assertEquals(expectedFirstNode, obtainedList.peekFirst());
+        assertEquals(expectedLastNode, obtainedList.peekLast());
+        assertEquals(expectedSize, obtainedList.size());
+
+    }
 }
